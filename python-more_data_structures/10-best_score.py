b9d1a76206958109e3b7e222073be07d57d71340
Codedfucha <a.gichure@alustudent.com>
2024-06-29 13:58:44 -0700

    best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,15 +1,3 @@
-#!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    
-    best_key = None
-    best_value = -float('inf')
-    
-    for key, value in a_dictionary.items():
-        if value > best_value:
-            best_value = value
-            best_key = key
-    
-    return best_key
-
+    max_score = max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
+    return max_score
