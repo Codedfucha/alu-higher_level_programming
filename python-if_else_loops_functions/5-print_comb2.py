4ceff630e8aed7b4a0e4eced8121a75938a9114b
Codedfucha <a.gichure@alustudent.com>
2024-06-29 07:13:23 -0700

    Fix script to print numbers from 00 to 99 with up to 10 numbers per line using one print function

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
-        print(f"0{i}", end=", " if i != 9 else "\n")
-    else:
-        print(f"{i}", end=", " if i != 99 else "\n")
+    print("{:02d}".format(i), end=", " if i != 99 else "\n")
