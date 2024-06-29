113e74591b219c103a6b5f75870991ef173f93df
Codedfucha <a.gichure@alustudent.com>
2024-06-29 10:36:24 -0700

    again

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.8
+import importlib.util
+
+if __name__ == "__main__":
+    spec = importlib.util.spec_from_file_location("hidden_4", "./hidden_4.pyc")
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    
+    names = [name for name in dir(module) if not name.startswith("__")]
+    names.sort()
+    
+    for name in names:
+        print(name)
