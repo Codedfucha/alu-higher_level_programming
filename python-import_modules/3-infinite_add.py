5caf5122edf86e51fdb1a25f69d5823a3d46862e
Codedfucha <a.gichure@alustudent.com>
2024-06-29 10:29:04 -0700

    ininite addittion

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 import sys
 
 if __name__ == "__main__":
-    argv = sys.argv[1:]  # Exclude the script name
-    total = sum(int(arg) for arg in argv)  # Sum all arguments converted to integers
+    argv = sys.argv[1:]
+    total = sum(int(arg) for arg in argv)
     print(total)
