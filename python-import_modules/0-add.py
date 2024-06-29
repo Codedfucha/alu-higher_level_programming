0f2d1fa495f8e0742e1bc40611a267d015d50895
Codedfucha <a.gichure@alustudent.com>
2024-06-29 10:11:14 -0700

    again

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,19 +4,22 @@ import os
 def check_readme():
     if os.path.exists("README.md"):
         if os.path.getsize("README.md") > 0:
-            print("README.md exists and is not empty")
+            return "README.md exists and is not empty"
         else:
-            print("README.md exists but is empty")
+            return "README.md exists but is empty"
     else:
-        print("README.md does not exist")
+        return "README.md does not exist"
 
 if __name__ == "__main__":
     from add_0 import add
 
     # Check if README.md exists and is not empty
-    check_readme()
+    readme_status = check_readme()
 
     # Addition operation
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    addition_result = "{} + {} = {}".format(a, b, add(a, b))
+
+    # Combined print statement
+    print("{}\n{}".format(readme_status, addition_result))
