0756a5002d6983339dabb589537123d0f21027b3
Codedfucha <a.gichure@alustudent.com>
2024-06-29 08:59:37 -0700

    power function

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def pow(a, b):
+    """
+    Computes a to the power of b and returns the value.
+
+    Args:
+        a: The base integer.
+        b: The exponent integer.
+
+    Returns:
+        The value of a ^ b.
+    """
+    return a ** b
