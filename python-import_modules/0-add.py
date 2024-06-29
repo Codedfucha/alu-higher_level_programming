13dc6042b8d9960866026fb2662b3c9f5d7bd7a0
Codedfucha <a.gichure@alustudent.com>
2024-06-29 09:56:51 -0700

    Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
+import os
+
+def check_readme():
+    if os.path.exists("README.md"):
+        if os.path.getsize("README.md") > 0:
+            print("README.md exists and is not empty")
+        else:
+            print("README.md exists but is empty")
+    else:
+        print("README.md does not exist")
+
 if __name__ == "__main__":
     from add_0 import add
 
+    # Check if README.md exists and is not empty
+    check_readme()
+
+    # Addition operation
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
