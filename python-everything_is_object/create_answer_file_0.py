9cf67486391e0155649cbf0a32fff6e60391a6d7
Codedfucha <a.gichure@alustudent.com>
2024-07-13 12:34:52 -0700

    preceed

---
+++ python-everything_is_object/create_answer_file_0.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# Open the file in write mode
+with open("0-answer.txt", "w") as file:
+    # Write the correct content followed by a newline character
+    file.write("type\n")
