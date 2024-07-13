d36bc2227adf7a5ba9b77f9ed4fb4a29c46fd293
Codedfucha <a.gichure@alustudent.com>
2024-07-13 05:31:50 -0700

    again

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""
+0-square.py
+Module to define the Square class.
+
+This module defines a simple Square class with no attributes or methods.
+"""
 class Square:
     pass
+    """
+    Defines a square.
+
+    This class doesn't have any attributes or methods defined yet.
+    """
