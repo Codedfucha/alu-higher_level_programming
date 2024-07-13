24380a8bfe831bd37ec0ce5ae1b92099c8adfffc
Codedfucha <a.gichure@alustudent.com>
2024-07-13 12:32:04 -0700

    right count

---
+++ python-everything_is_object/create_answer_file_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# Open the file in write mode
+with open("2-answer.txt", "w") as file:
+    # Write the correct content followed by a newline character
+    file.write("No\n")
+
