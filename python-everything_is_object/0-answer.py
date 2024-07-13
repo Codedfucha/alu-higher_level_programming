ffabd8dd60c5a4d868bb3baffecbd545b3e912d0
Codedfucha <a.gichure@alustudent.com>
2024-07-13 11:29:49 -0700

    who am i?

---
+++ python-everything_is_object/0-answer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def type_print(obj):
+    """
+    Prints the type of an object.
+    """
+    print(type(obj))
+
+# Test the function
+if __name__ == "__main__":
+    type_print(5)
