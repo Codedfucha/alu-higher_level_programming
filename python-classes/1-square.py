9924eaff784260a29420b54735561a24497e7f44
Codedfucha <a.gichure@alustudent.com>
2024-07-13 05:52:17 -0700

    updated square

---
+++ python-classes/1-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+1-square.py
+Module to define the Square class.
+"""
+
+class Square:
+    """
+    Defines a square.
+
+    Attributes:
+        __size (int): Private attribute representing the size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a square with a given size.
+
+        Args:
+            size (int): Size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        self.__size = size
