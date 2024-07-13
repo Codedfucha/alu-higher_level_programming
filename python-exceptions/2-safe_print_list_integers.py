fd54152fb92daae85e843cae10ab908c8e9fccba
Codedfucha <a.gichure@alustudent.com>
2024-07-13 04:10:47 -0700

    printlist

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -10,6 +10,7 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             continue
         except IndexError:
-            break
+            print()
+            raise
     print()
     return count
