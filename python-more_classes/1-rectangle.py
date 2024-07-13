eb5e380933f903cd6f7a3af1a27c1530579bcf24
Codedfucha <a.gichure@alustudent.com>
2024-07-13 07:45:44 -0700

    pep8 check

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-This module defines a class Rectangle with private instance attributes width and height.
+This module defines a class Rectangle with private instance 
+attributes width and height.
 """
+
 
 class Rectangle:
     """
