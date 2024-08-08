641958dd4e9672bfd96eb11c9333aba28969ab51
Codedfucha <a.gichure@alustudent.com>
2024-08-08 10:09:10 -0700

    test

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
+"""__summary__
+- Write a Python script that fetches https://alu-intranet.hbtn.io/status
+- using the urllib package.
 """
-This script fetches https://alu-intranet.hbtn.io/status
-using urllib package and displays the body of the response.
-"""
-
 import urllib.request
 
-if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
+
+
+if __name__ == '__main__':
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
