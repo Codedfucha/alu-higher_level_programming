1473e06ae0e3cfef2b2bb615e6c0ccfd951954b9
Codedfucha <a.gichure@alustudent.com>
2024-08-08 08:40:50 -0700

    Response header2

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
-"""Fetches a URL and displays the value of the X-Request-Id variable from the response headers."""
+"""Fetches a URL and displays the value of the X-Request-Id variable from the response headers. "
+This script takes a URL as a command-line argument, sends an HTTP request to the URL,
+and displays the value of the 'X-Request-Id' variable found in the header of the response.
+
+Usage:
+    ./1-hbtn_header.py <URL>
+
+Arguments:
+    <URL> : The URL to which the request is sent. This URL must be accessible and should respond with appropriate headers.
+
+Dependencies:
+    - urllib.request : For sending HTTP requests and handling responses.
+
+Example:
+    $ ./1-hbtn_header.py https://intranet.hbtn.io
+    ade2627e-41dd-4c34-b9d9-a0fa0f47b237
+"""
 import sys
 import urllib.request
 import urllib.error
