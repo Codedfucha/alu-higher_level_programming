bbeeba01cb315271dd0999045e8ca773156ad3d6
Codedfucha <a.gichure@alustudent.com>
2024-08-08 08:15:39 -0700

    2nd

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
-A Python script that fetches https://intranet.hbtn.io/status
-using the urllib package.
+This script fetches https://alu-intranet.hbtn.io/status
+using urllib package and displays the body of the response.
 """
 
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    
+    url = "https://alu-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
